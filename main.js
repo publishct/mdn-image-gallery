bba51f81393034ad4f83ddf6d3b157fd39b3fd77
Cynthia Trujillo <developercynthia@gmail.com>
2020-12-04 18:58:48 -0600

    Handle the darken/lighten button

--- main.js
+++ main.js
@@ -24,3 +24,19 @@ for (let i = 1; i < 6; i++) {
 };
 
 /* Wiring up the Darken/Lighten button */
+
+function bgChange() {
+
+    if (btn.getAttribute('class') === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.0)';
+    }
+   
+};
+
+btn.onclick = bgChange;
